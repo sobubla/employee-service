12f2d2c926f16d1a24ad1e74eed6a8105cd20463
Soumya Bhattacharya <soumya.bhattacharya@metlife.com>
2019-03-06 17:18:33 +0530

    first commit

---
+++ src/main/java/com/ibm/micronaut/services/employee_service/EmployeeApplication.java
@@ -0,0 +1,23 @@
+package com.ibm.micronaut.services.employee_service;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employees Management",
+                version = "1.0",
+                description = "Employee API",
+                contact = @Contact(url = "https://piotrminkowski.wordpress.com", name = "Piotr Mińkowski", email = "piotr.minkowski@gmail.com")
+        )
+)
+
+public class EmployeeApplication {
+	
+	public static void main(String[] args) {
+        Micronaut.run(EmployeeApplication.class);
+    }
+
+}
